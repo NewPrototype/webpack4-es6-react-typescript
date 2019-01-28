5af7b3ba45bc2c2e13fc9bce6b07d9db26bfcd0b
liujunxiong <heicao.ljx@raycloud.com>
2019-01-28 11:01:43 +0800

    test git

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -18,8 +18,8 @@ class Home extends React.Component<any, {}> {
   render() {
     return (
       <div className="home">
-        主页面23
-        <span onClick={this.init}>--------12</span>
+        test 
+        <span onClick={this.init}>--------12</span>\
       </div>
     );
   }
